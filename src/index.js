bf2878cab1a16b26a71658cb6fdf200ff21079e9
Epshtein Dmitry <letuver@yahoo.com>
2022-01-12 16:57:03 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import validation from "./modules/validation";
 import tabs from "./modules/tabs";
 import slider from "./modules/slider";
 import calc from "./modules/calc";
+import sendForm from "./modules/sendForm";
 
 timers("1 january 2022");
 menu();
@@ -13,3 +14,6 @@ validation();
 tabs();
 slider();
 calc(100);
+sendForm("form1");
+sendForm("form2");
+sendForm("form3");
---
+++ src/modules/sendForm.js
@@ -0,0 +1,95 @@
+const sendForm = idForm =>{
+    const form = document.getElementById(idForm);
+
+    const statusBlock = document.createElement("div");
+    const loadText = "Загрузка";
+    const errorText = "Ошибка";
+    const successText = "Данные отправлены успешно";
+    
+
+    const validate = list =>{
+        let success = true;
+        list.forEach(input => {
+            if(input.type == 'text'){                
+                const letters = /^[А-Яа-яёЁ0-9]+$/;
+                if(!input.value.match(letters)){
+                    success = false;
+                    return success;
+                }    
+            }
+            else if(input.type == 'tel'){
+                const letters = /\d+/;
+                if(!input.value.match(letters)){
+                    success = false;
+                    return success;
+                }
+
+            }else if(input.type == 'email'){
+                const letters = /^[a-zA-Z0-9@\-\_\.\!\~\*\']+$/;
+                if(!input.value.match(letters)){
+                    success = false;
+                    return success;
+                }
+            }
+        });
+
+        return success;
+    };
+
+    let url = 'https://jsonplaceholder.typicode.com/posts';
+    const sendData = data =>{
+        return fetch(url, {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers:{
+                "Content-Type": "aplication/json"
+            }
+        }).then(res => res.json());
+    };
+
+    const submitForm = () => {
+        const formElements = form.querySelectorAll("input");
+        const formData = new FormData(form);
+        const formBody = {};
+
+        statusBlock.textContent = loadText;
+        form.append(statusBlock);
+
+        formData.forEach((val, key) => {
+            formBody[key] = val;
+        });
+
+        if(validate(formElements)){
+            sendData(formBody).
+            then(data => {
+                statusBlock.textContent = successText;
+                setTimeout(() => statusBlock.textContent = "", 3000);
+                formElements.forEach(input => {
+                    input.value = "";
+                });
+                console.log(data);
+            }).catch(error => {
+                statusBlock.textContent = errorText;
+            });
+        }
+        else{
+            console.log("data is not valid");
+        }   
+    };
+
+    try{
+        if(!form){
+            throw new Error("Верните форму на место!");
+        }
+        form.addEventListener("submit", e =>{
+            e.preventDefault();
+
+            submitForm();
+    });
+    }
+    catch(error){
+        console.log(error.message);
+    }
+    
+};
+export default sendForm;
